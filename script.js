37cfdb3b10464ac484f7fa2845a8753ce2d619db
Akhilesh Sharma <vidhi.sydney07@gmail.com>
2022-09-26 18:46:53 +1000

    fixed the code for changing http to https also made  changes to display today's date

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function fetchInfo() {
     }
 
     $('#regionName').text(location);
-    var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + location + '&limit=5&appid=' + api;
+    var requestUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + location + '&limit=5&appid=' + api;
     $('#location').val("");
     fetch(requestUrl)
         .then(function (response) {
@@ -67,9 +67,9 @@ function fethchWeatherinfo(locatationLatitude, locationLongitude) {
                 response.json().then(function (data) {
                     console.log(data);
                     var weatherImage = document.getElementById("imageToday");
+                    var forecastDate = document.getElementById('todayDate');
                     weatherImage.src = "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png"
                     weatherImage.alt = "demo icon"
-                    console.log(weatherImage.src);
                     $('#temp').text('Temperature:   ' + data.main.temp + ' \u00B0 C');
                     //changing the temperature into km/hr
                     $('#Wind').text('Wind:  ' + data.wind.speed + 'm/s');
@@ -129,13 +129,14 @@ function fiveDayForecast(latitude, longitude) {
                 var windSpeed = document.getElementById('Wind' + j);
                 var humidity = document.getElementById('humidity' + j);
                 var forecastDate = document.getElementById('date' + j);
-                weatherImage.src = "http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png"
+                weatherImage.src = "https://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png"
                 weatherImage.alt = "demo icon"
                 temperature.textContent = 'Temperature :' + data.list[i].main.temp + ' \u00B0 C';
                 windSpeed.textContent = 'Wind Speed   :' + data.list[i].wind.speed + 'm/s';
                 humidity.textContent = 'Humidity   :' + data.list[i].main.humidity + '%';
                 forecastDate.textContent = 'Date :' + moment(data.list[i].dt_txt, 'YYYY-MM-DD hh:mm:ss').format("MMM Do, YYYY");
                 forecastCard.classList.add('border', 'border-dark');
+                $('#todayDate').text('Today :   ' + moment(data.list[0].dt_txt, 'YYYY-MM-DD hh:mm:ss').format("MMM Do, YYYY"))
 
             }
 
